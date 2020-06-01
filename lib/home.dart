806121c0e5ddb43da788bf2f0431df3c31b42918
preshen-goobiah <goobiah101@gmail.com>
2020-06-01 20:19:41 +0200

    Add Drawer for quick options

--- lib/home.dart
+++ lib/home.dart
@@ -24,7 +24,100 @@ class _HomePageState extends State<HomePage>
     // TODO: implement build
     return Scaffold(
       appBar: new AppBar(
-        title: Text('ShopApp2')
+        backgroundColor: Colors.redAccent,
+        title: Text('FashApp'),
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.search, color: Colors.white), onPressed: (){}),
+          new IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white), onPressed: (){})
+        ],
+      ),
+
+      drawer: new Drawer(
+
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader
+                (
+                accountName: Text('Matt Lecache'),
+                accountEmail: Text('matt@gmail.com'),
+
+                currentAccountPicture: GestureDetector(
+                  child: new CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Icon(Icons.person, color: Colors.white)
+                  )
+                ),
+              decoration: new BoxDecoration(
+                color: Colors.redAccent
+              )
+            ),
+//          Body
+
+          InkWell(
+            onTap: (){},
+            child: ListTile(
+              title: Text('Home Page'),
+              leading: Icon(Icons.home),
+
+            ),
+          ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.person),
+
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket),
+
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard),
+
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Favorites'),
+                leading: Icon(Icons.favorite),
+
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+
+              ),
+            )
+
+          ],
+        )
+
       ),
     );
   }
