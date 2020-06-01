9081a889c6a3dc3ee17ab06b062ab86a63943277
preshen-goobiah <goobiah101@gmail.com>
2020-06-01 19:07:47 +0200

    Build HomePage with stateful widget

---
+++ lib/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+/*class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+        color: Colors.lightBlue,
+        child: Text("Hey", style: TextStyle(color: Colors.white))
+    );
+  }
+}*/
+
+
+class HomePage extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('ShopApp')
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+// stateful widget for changing content like a product catalog
+// stateless widget for static content like a login screen
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomePage()
+    )
+  );
+}
+
