385984a3814a3434e34e7a86b0ca72a0ad7fab5d
preshen-goobiah <goobiah101@gmail.com>
2020-06-06 21:19:30 +0200

    Add cart details

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -226,6 +226,21 @@ class _ProductDetailsState extends State<ProductDetails> {
               Padding(padding: EdgeInsets.all(5),
                 child: new Text("?"),)
             ],
+          ),
+
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Text("Similar Products"),
+          ),
+
+          // Similar products section
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 360,
+                child: Similar_products(),
+            ),
           )
         ],
       )
@@ -244,41 +259,17 @@ class _Similar_productsState extends State<Similar_products> {
   // ignore: non_constant_identifier_names
   List product_list = [
     {
-      'name': 'Blazer',
-      'picture': 'images/products/blazer1.jpeg',
+      'name': 'Show',
+      'picture': 'images/products/shoe1.jpg',
       'old_price': 120,
       'price': 85,
     },
     {
-      'name': 'Dress',
-      'picture': 'images/products/dress1.jpeg',
+      'name': 'Skirt - 2',
+      'picture': 'images/products/skt2.jpeg',
       'old_price': 100,
       'price': 55,
-    },
-    {
-      'name': 'Heels',
-      'picture': 'images/products/hills1.jpeg',
-      'old_price': 100,
-      'price': 55,
-    },
-    {
-      'name': 'Skirt',
-      'picture': 'images/products/skt1.jpeg',
-      'old_price': 100,
-      'price': 55,
-    },
-    {
-      'name': 'Pants',
-      'picture': 'images/products/pants1.jpeg',
-      'old_price': 100,
-      'price': 55,
-    },
-    {
-      'name': 'Blazer - 2',
-      'picture': 'images/products/blazer2.jpeg',
-      'old_price': 100,
-      'price': 55,
-    },
+    }
 
 
   ];
@@ -288,7 +279,7 @@ class _Similar_productsState extends State<Similar_products> {
         itemCount: product_list.length,
         gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index){
-          return Single_prod(
+          return Similar_Single_prod(
             prod_name: product_list[index]['name'],
             prod_picture: product_list[index]['picture'],
             prod_old_price: product_list[index]['old_price'],
@@ -297,4 +288,57 @@ class _Similar_productsState extends State<Similar_products> {
         });
   }
 }
+
+class Similar_Single_prod extends StatelessWidget {
+  final prod_name;
+  final prod_picture;
+  final prod_old_price;
+  final prod_price;
+
+  Similar_Single_prod({
+    this.prod_name,
+    this.prod_picture,
+    this.prod_old_price,
+    this.prod_price
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Hero(
+          // tag must be unique
+            tag: prod_name,
+            child: Material(
+                child: InkWell(onTap: () => Navigator.of(
+                    context).push(new MaterialPageRoute(builder: (context) => new ProductDetails(
+                  // passing the values of the product to the product details page
+                  product_detail_name: prod_name,
+                  product_detail_old_price: prod_old_price,
+                  product_detail_new_price: prod_price,
+                  product_detail_picture: prod_picture,
+                ))),
+                    child: GridTile(
+                      footer: Container(
+                          color: Colors.white70,
+                          child: Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Text(prod_name,
+                                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),)
+                              ),
+                              new Text("\$${prod_price}",
+                                style: TextStyle(color: Colors.redAccent, fontSize: 16, fontWeight: FontWeight.bold),)
+
+                            ],
+                          )
+                      ),
+                      child: Image.asset(prod_picture,
+                        fit: BoxFit.cover,),
+                    )
+                )
+            )
+        ));
+  }
+}
+
 
