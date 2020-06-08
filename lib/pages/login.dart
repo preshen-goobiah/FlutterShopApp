c804e0cd472494c07a33b39e491d4d1a32d02547
preshen-goobiah <goobiah101@gmail.com>
2020-06-08 20:44:05 +0200

    fix firebase_core_web not found and dex error

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -47,9 +47,70 @@ class _LoginState extends State<Login> {
 
   }
 
+  Future handleSignIn() async{
+
+    preferences = await SharedPreferences.getInstance();
+    setState(() {
+      loading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignin.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication = await googleUser.authentication;
+
+    AuthCredential credential = GoogleAuthProvider.getCredential(idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    FirebaseUser firebaseUser = (await firebaseAuth.signInWithCredential(credential)).user;
+
+    if(firebaseUser != null)
+      {
+        final QuerySnapshot result = await Firestore.instance.collection(("users")).where("id",
+            isEqualTo: firebaseUser.uid).getDocuments();
+        final List<DocumentSnapshot> documents = result.documents;
+
+        if(documents.length == 0 )
+          {
+            // insert user into firebase collection
+            Firestore.instance
+                .collection("users")
+                .document(firebaseUser.uid)
+                .setData({
+              "id" : firebaseUser.uid,
+              "username": firebaseUser.displayName,
+              "photoUrl": firebaseUser.photoUrl
+            });
+
+            await preferences.setString("id", firebaseUser.uid);
+            await preferences.setString("username", firebaseUser.displayName);
+            await preferences.setString("photoUrl", firebaseUser.photoUrl);
+          }
+          else
+            {
+              await preferences.setString("id", documents[0]['id']);
+              await preferences.setString("username", documents[0]['username']);
+              await preferences.setString("photoUrl", documents[0]['photoUrl']);
+            }
+
+          Fluttertoast.showToast(msg: "Login successful");
+
+          setState(() {
+            loading=false;
+          });
+      }
+    else
+      {
+        Fluttertoast.showToast(msg: "Login failed");
+
+      }
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: new AppBar(
+    )
+    );
   }
 
 
